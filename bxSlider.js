f83a05536bcc830968288a57cef186092addb4b4
Pedro Villarreal <pestrexv@gmail.com>
2023-03-07 18:16:14 -0600

    cafetería

---
+++ bxSlider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $('.galeria').bxSlider({
+        auto: true,
+        mode: 'fade',
+        captions: false,
+        slideWidth: 800,
+        responsive: true,
+        pager: true
+    });
+
+});
